27bf00e5b36593c953ac3fb1c909e4b2d52a8097
Nguyễn Quang Lộc <nguyenquangloc0306@gmail.com>
2024-07-30 07:15:27 +0700

    Đã chỉnh sửa lại Sign In Form !

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -1,9 +1,80 @@
-'use client';
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+const formSchema = z.object({
+  username: z.string().min(1, 'Username is required').min(8,'Username must be at least 8 characters long'),
+  password: z.string().min(1 , 'Password is required').min(8,'Password must be at least 8 characters long'),
+});
 
 const SignInForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log('Form submitted:', values);
+  };
+  
+
   return (
-    <div>SignInForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Password" type="password" {...field} />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="w-full mt-6" type="submit">Sign In</Button>
+        </form>
+      <div className='mx-auto my-4 flex w-full items-center justify-evenly
+      before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
+      after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400'>
+        or
+      </div>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you don&apos;t have an account, please&nbsp;
+        <Link className="text-blue-400 hover:underline" href='/sign-up'>Sign up</Link>
+      </p>
+    </Form>
   )
 }
 
-export default SignInForm
+export default SignInForm;
